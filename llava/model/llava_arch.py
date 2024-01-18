f3f862fec7f35cec75c077d6f57083b6c3d9b207
Shao-Yen Tseng <shao-yen.tseng@intel.com>
2024-01-18 00:11:51 +0000

    Update to v1.13.0

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -152,8 +152,9 @@ class LlavaMetaForCausalLM(ABC):
             if num_images == 0:
                 cur_image_features = image_features[cur_image_idx]
                 cur_input_embeds_1 = self.get_model().embed_tokens(cur_input_ids)
-                # cur_input_embeds = torch.cat([cur_input_embeds_1, cur_image_features[0:0]], dim=0)
-                new_input_embeds.append(cur_input_embeds_1)
+                cur_input_embeds = torch.cat([cur_input_embeds_1, cur_image_features[0:1]], dim=0)
+                cur_input_embeds = cur_input_embeds[0:-1]
+                new_input_embeds.append(cur_input_embeds)
                 new_labels.append(labels[batch_idx])
                 cur_image_idx += 1
                 continue
